af802929ee31edf581cb79e06b6c38caa18ab390
AlexOnomy <alex@onomy.io>
2021-10-11 09:20:44 -0700

    Closes #215 Update Minimum Purchase Error Text

--- src/constants/NotificationMessages.js
+++ src/constants/NotificationMessages.js
@@ -6,7 +6,7 @@ export const NOTIFICATION_MESSAGES = {
     incorrectAmountFormat: 'wNOM amount format is not correct',
     rejectedTransaction: 'Your transaction is rejected',
     failedTransaction: 'Your transaction failed',
-    lowBid: `You have entered insufficient amount to cover gas fee. Since the commission is already included to the specified amount, please increase amount to execute transaction.`,
+    lowBid: `Your purchase amount must be more than the cumulative transaction and gas fee.  Please increase your purchase amount to successfully submit your transaction.`,
   },
   success: {
     approvedBridgeTokens: amount => `Transaction Completed! ${amount} wNOM approved to swap.`,
