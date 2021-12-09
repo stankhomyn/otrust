5a7685b8c055d52ce93afd0f08696a18f2bd3590
AlexOnomy <alex@onomy.io>
2021-12-09 06:50:26 +0000

    Default to devnet without .env

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,10 +1,10 @@
 export const {
   REACT_APP_GRAPHQL_ENDPOINT = 'https://api.thegraph.com/subgraphs/name/onomyprotocol/ograph',
   REACT_APP_ETHERSCAN_API_KEY = '3CVJBSFD6KVNBTNFCBN2T2QHRVYP1K81YB',
-  REACT_APP_ONOMY_RPC_URL = 'https://rpc-testnet.onomy.online',
-  REACT_APP_ONOMY_REST_URL = 'https://rest-testnet.onomy.online',
-  REACT_APP_ONOMY_WS_URL = 'wss://rpc-testnet.onomy.online',
+  REACT_APP_ONOMY_RPC_URL = 'https://rpc-devnet.onomy.online',
+  REACT_APP_ONOMY_REST_URL = 'https://rest-devnet.onomy.online',
+  REACT_APP_ONOMY_WS_URL = 'wss://rpc-devnet.onomy.online',
   REACT_APP_BONDING_NOM_CONTRACT_ADDRESS = '0x52A4A4FBB36A2f6A83dBf7De9425C1ec5FF528Da',
   REACT_APP_WNOM_CONTRACT_ADDRESS = '0xe7c0fd1f0A3f600C1799CD8d335D31efBE90592C',
-  REACT_APP_GRAVITY_CONTRACT_ADDRESS = '0x119999cf67269C29CEC39337106e00bBbcd68bf9',
+  REACT_APP_GRAVITY_CONTRACT_ADDRESS = '0x0F23c3f0C77582a5dB7fB3D61097B619982fb32f',
 } = process.env;
