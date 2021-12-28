136c48219d0f452a1da6fe4ffc414938bf846a2b
AlexOnomy <90110473+AlexOnomy@users.noreply.github.com>
2021-12-28 04:31:09 -0800

    Don't show Bridged Balance in sidebar (#237)
    * Isolate SidebarBridgedBalance to own component
    
    * Only show bridged nom when REACT_APP_SHOW_BRIDGED_NOM is set

--- src/components/Sidebar/SidebarBalances.jsx
+++ src/components/Sidebar/SidebarBalances.jsx
@@ -1,276 +1,25 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 // import ModalComponent from 'components/Modals/components/ModalComponent';
 import BridgeSwapMain from 'components/Modals/components/BridgeSwapMain';
 import { PrimaryButton } from 'components/Modals/styles';
-import { withTrimmedWrapper } from 'components/UI';
-import { responsive } from 'theme/constants';
-import { useOnomy } from 'context/chain/OnomyContext';
-import { NomBalanceDisplay } from 'components/NomBalanceDisplay';
-
-const Balances = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-
-  padding: 24px 40px;
-
-  border-bottom: 1px solid ${props => props.theme.colors.bgHighlightBorder};
-
-  @media screen and (max-width: ${responsive.laptop}) {
-    padding: 24px;
-  }
-
-  @media screen and (max-width: ${responsive.tablet}) {
-    flex-direction: row;
-    align-items: center;
-    grid-column: 1/3;
-    grid-row: 2/3;
-
-    border: none;
-  }
-
-  @media screen and (max-width: ${responsive.tabletSmall}) {
-    gap: 40px;
-  }
-
-  @media screen and (max-width: ${responsive.smartphoneLarge}) {
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-
-    padding: 20px;
-    gap: 0px;
-  }
-
-  strong {
-    display: block;
-    margin-bottom: 10px;
-
-    font-weight: 400;
-    color: ${props => props.theme.colors.textSecondary};
-  }
-`;
-
-const BalanceNumber = styled.div`
-  font-family: 'Bebas Neue', sans-serif;
-  font-size: 28px;
-  font-weight: ${props => (props.strong ? 700 : 400)};
-
-  > small {
-    margin-left: 5px;
-
-    font-family: 'Poppins', sans-serif;
-    color: ${props => props.theme.colors.textSecondary};
-    font-size: 11px;
-  }
-
-  @media screen and (max-width: ${responsive.laptop}) {
-    font-size: 20px;
-  }
-`;
-
-const Balance = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const BalancePrice = styled.div``;
-
-const BalanceHint = styled.div`
-  position: relative;
-`;
-
-const SecondaryIcon = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  height: 40px;
-  width: 40px;
-
-  background-color: ${props => props.theme.colors.bgHighlightBorder};
-  border-radius: 8px;
-  border: none;
-
-  font-size: 20px;
-  font-weight: 500;
-  color: ${props => props.theme.colors.iconsSecondary};
-
-  cursor: pointer;
-  user-select: none;
-
-  @media screen and (max-width: ${responsive.laptop}) {
-    width: 32px;
-    height: 32px;
-
-    font-size: 14px;
-  }
-
-  @media screen and (max-width: ${responsive.tablet}) {
-    margin-left: 24px;
-  }
-
-  &:hover {
-    background-color: ${props => props.theme.colors.iconsNormal};
-    color: ${props => props.theme.colors.bgDarken};
-  }
-
-  &:active {
-    background-color: ${props => props.theme.colors.bgHighlightBorder_darken};
-  }
-`;
-
-const Approved = styled.div`
-  display: inline-block;
-
-  padding: 4px 8px;
-  margin-top: 6px;
-
-  position: relative;
-
-  background-color: #2a303f;
-  border-radius: 6px;
-
-  color: ${props => props.theme.colors.highlightBlue};
-  font-size: 12px;
-  font-weight: 400;
-  font-family: Poppins, sans-serif;
-
-  /* @media screen and (max-width: ${responsive.tablet}) {
-    margin: 0 0 0 12px;
-  } */
-
-  svg {
-    position: absolute;
-    right: 4px;
-    top: 2px;
-
-    cursor: pointer;
-    * {
-      fill: currentColor;
-    }
-  }
-`;
-
-const WithdrawBtnWrapper = styled.div`
-  margin-top: 20px;
-  min-width: 150px;
-
-  @media screen and (max-width: ${responsive.tablet}) {
-    margin: 0 0 0 auto;
-  }
-
-  @media screen and (max-width: ${responsive.smartphoneLarge}) {
-    padding-top: 10px;
-    margin: 0;
-  }
-`;
-
-const TooltipWrapper = styled.div`
-  display: ${props => (props.active ? 'block' : 'none')};
-
-  width: 360px;
-  padding: 32px 40px;
-
-  position: absolute;
-  right: 65px;
-  top: -35px;
-
-  background-color: ${props => props.theme.colors.bgHighlightBorder};
-  border-radius: 8px;
-  box-shadow: 0 5px 50px 0 rgba(0, 0, 0, 0.36);
-
-  z-index: 1;
-  cursor: pointer;
-
-  &:after {
-    content: '';
-    display: block;
-
-    position: absolute;
-    right: -20px;
-    top: 25px;
-
-    width: 0;
-    height: 0;
-    border-top: 30px solid transparent;
-    border-bottom: 30px solid transparent;
-
-    border-left: 30px solid ${props => props.theme.colors.bgHighlightBorder};
-  }
-
-  @media screen and (max-width: ${responsive.tablet}) {
-    width: 300px;
-    padding: 24px;
-
-    right: -135px;
-    top: 50px;
-
-    &:after {
-      right: 130px;
-      top: -35px;
-
-      border-top: 20px solid transparent;
-      border-bottom: 20px solid ${props => props.theme.colors.bgHighlightBorder};
-      border-left: 20px solid transparent;
-      border-right: 20px solid transparent;
-    }
-  }
-
-  @media screen and (max-width: ${responsive.smartphoneLarge}) {
-    width: 240px;
-    padding: 16px 24px;
-
-    right: 50px;
-    top: -55px;
-
-    &:after {
-      right: -30px;
-      top: 50px;
-
-      border-top: 20px solid transparent;
-      border-bottom: 20px solid transparent;
-      border-left: 20px solid ${props => props.theme.colors.bgHighlightBorder};
-      border-right: 20px solid transparent;
-    }
-  }
-`;
-
-const TooltipCaption = styled.h5`
-  margin-bottom: 16px;
-
-  font-size: 14px;
-  font-weight: 500;
-`;
-
-const TooltipDesc = styled.p`
-  color: ${props => props.theme.colors.textSecondary};
-`;
-
-export const TrimmedApproved = withTrimmedWrapper(({ value }) => (
-  <Approved>
-    <span>{value} approved</span>
-  </Approved>
-));
-
-function Hint({ children }) {
-  const [active, setActive] = useState(false);
-
-  return (
-    <BalanceHint onPointerOver={() => setActive(true)} onPointerLeave={() => setActive(false)}>
-      <TooltipWrapper active={active}>{children}</TooltipWrapper>
-      <SecondaryIcon active={active}>i</SecondaryIcon>
-    </BalanceHint>
-  );
-}
+import {
+  Balances,
+  Balance,
+  BalancePrice,
+  BalanceNumber,
+  Hint,
+  TooltipCaption,
+  TooltipDesc,
+  TrimmedApproved,
+  WithdrawBtnWrapper,
+} from './SidebarStyles';
+import { SidebarBridgedBalance } from './SidebarBridgedBalance';
+import { REACT_APP_SHOW_BRIDGED_NOM } from 'constants/env';
 
 export default function SidebarBalances({ strong, weak, strongBalance, weakBalance, allowance }) {
   // const { handleModal } = useModal();
   const [showBridge, setShowBridge] = useState(false);
-  const { amount: nomBalance, address: nomAddress, bridgeProgress } = useOnomy();
 
   return (
     <>
@@ -307,25 +56,7 @@ export default function SidebarBalances({ strong, weak, strongBalance, weakBalan
           </Hint>
         </Balance>
 
-        {nomAddress && (
-          <Balance>
-            <BalancePrice>
-              <strong>NOM Balance</strong>
-              <BalanceNumber strong>
-                <NomBalanceDisplay value={nomBalance} />
-                {bridgeProgress === null ? (
-                  <small> = $16,208.04</small>
-                ) : (
-                  <small>{bridgeProgress.toFixed(2)}%</small>
-                )}
-              </BalanceNumber>
-            </BalancePrice>
-            <Hint>
-              <TooltipCaption>NOM Balance</TooltipCaption>
-              <TooltipDesc>This shows your total NOM balance on the Onomy chain</TooltipDesc>
-            </Hint>
-          </Balance>
-        )}
+        {REACT_APP_SHOW_BRIDGED_NOM && <SidebarBridgedBalance />}
 
         <WithdrawBtnWrapper>
           <PrimaryButton style={{ width: '100%' }} onClick={() => setShowBridge(true)}>
